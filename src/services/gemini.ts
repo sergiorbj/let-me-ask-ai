aaf6a6ccf6b1514fa3a30c4ba2e44ce0b6aab900
Sergio Bernardi <sergiorbj93@gmail.com>
2025-07-25 00:20:47 -0300

    feat: audio transcription endpoint

---
+++ src/services/gemini.ts
@@ -0,0 +1,47 @@
+import { GoogleGenAI } from '@google/genai';
+import { env } from '../env.ts';
+
+const gemini = new GoogleGenAI({
+  apiKey: env.GEMINI_API_KEY,
+});
+
+const model = 'gemini-2.5-flash';
+
+export async function transcribeAudio(audioAsBase64: string, mimeType: string) {
+  const response = await gemini.models.generateContent({
+    model,
+    contents: [
+      {
+        text: 'Transcribe given audio to US English text. Be precise and natural in transcription. Keep proper punctuation and break the text into paragraphs when necessary.',
+      },
+      {
+        inlineData: {
+          mimeType,
+          data: audioAsBase64,
+        },
+      },
+    ],
+  });
+
+  if (!response.text) {
+    throw new Error('It was not possible to convert the audio');
+  }
+
+  return response.text;
+}
+
+export async function generateEmbeddings(text: string) {
+  const response = await gemini.models.embedContent({
+    model: 'text-embedding-004',
+    contents: [{ text }],
+    config: {
+      taskType: 'RETRIEVAL_DOCUMENT',
+    },
+  });
+
+  if (!response.embeddings?.[0].values) {
+    throw new Error('It was not possible to generate the embeddings.');
+  }
+
+  return response.embeddings[0].values;
+}
